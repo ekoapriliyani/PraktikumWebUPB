72629d94bb721743dc024f3715e9f6c644293fc9
ekoapriliyani <72810997+ekoapriliyani@users.noreply.github.com>
2025-01-10 15:23:06 +0700

    first commit

---
+++ Lat1_3_312310675.php
@@ -0,0 +1,11 @@
+<?php
+$addition = 2 + 4;
+$substraction = 6 - 2;
+$multiplication = 5 * 3;
+$division = 15 / 3;
+$modulus = 5 % 2;
+echo "penambahan: 2 + 4 = $addition <br/>";
+echo "penguranagan: 6 - 2 = $substraction <br/>";
+echo "perkalian: 5 * 3 = $multiplication <br/>";
+echo "pembagian: 15 / 3 = $division <br/>";
+echo "pembagian sisa: 5 % 2 = $modulus";
